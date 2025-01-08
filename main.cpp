47c5ddd44c9c7a06e702b6dcfc8d38dacaf5a065
Dylan Mwakasekele <Dylanmwakasekele@gmail.com>
2025-01-08 16:34:41 +0000

    First window

--- main.cpp
+++ main.cpp
@@ -2,6 +2,46 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+using namespace std;
+
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height); 
+
 int main() {
-	return 0;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+	    cout << "Failed to create GLFW window" << endl;
+	    glfwTerminate();
+	    return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+	    cout << "Failed to initialize GLAD" << endl;
+	    return -1;
+	} 
+
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);  
+  	
+	while(!glfwWindowShouldClose(window)) {
+	    glfwSwapBuffers(window);
+	    glfwPollEvents();    
+	}
+
+  	glfwTerminate();
+    return 0;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
 }
